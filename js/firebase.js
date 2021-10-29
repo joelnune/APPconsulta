be63f82164aefe7bb8c8b26272edaac333131f16
joelnune <91698993+joelnune@users.noreply.github.com>
2021-10-29 19:49:00 -0300

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyA5t49U_R2gnl3GkjWxIZrwt1c_Dc-qyWY",
+    authDomain: "appagendamentos-af14b.firebaseapp.com",
+    projectId: "appagendamentos-af14b",
+    storageBucket: "appagendamentos-af14b.appspot.com",
+    messagingSenderId: "411539221333",
+    appId: "1:411539221333:web:dbfabf09f1f200be7a5ddc",
+    measurementId: "G-TQGLDMH0VH"
+ };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
